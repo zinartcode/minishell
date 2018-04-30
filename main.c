0dc0ed483b9b596b1d9014e347784d7cb9c2be2c
Artur Zinnatullin <zinartinc@gmail.com>
2018-04-30 16:21:09 -0700

    read commands

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: azinnatu <azinnatu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/29 16:29:15 by azinnatu          #+#    #+#             */
-/*   Updated: 2018/04/30 16:14:36 by azinnatu         ###   ########.fr       */
+/*   Updated: 2018/04/30 16:20:11 by azinnatu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,19 +33,19 @@ int		main(int ac, char **av, char **envp)
 	{
 		ft_putstr_fd(GRN"m_sh$> "NRM, 1);
 		cmd = readinput();
-		print_env(cmd);
+		// print_env(cmd);
 		if (cmd[0] && ft_strcmp(cmd[0], "exit") == 0)
 			exit(0);
 		else if (cmd[0] && ft_strcmp(cmd[0], "env") == 0)
 			print_env(env);
 		else if (cmd[0] && ft_strcmp(cmd[0], "setenv") == 0)
-			ft_printf("%s\n", "run setenv");
+			ft_printf("%s\n", "run setenv"); // envp = ft_setenv(&envp, env);
 		else if (cmd[0] && ft_strcmp(cmd[0], "unsetenv") == 0)
-			ft_printf("%s\n", "run unsetenv");
+			ft_printf("%s\n", "run unsetenv"); //envp = ft_unsetenv(&envp, env);
 		else if (cmd[0] && ft_strcmp(cmd[0], "cd") == 0)
-			ft_printf("%s\n", "run cd");
+			ft_printf("%s\n", "run cd"); //ft_cd(env, envp);
 		else
-			ft_printf("%s\n", "else");
+			ft_printf("%s\n", "else"); //ft_getcommand(envp, env);
 	}
 
 	// while (get_next_line(0, &str))
@@ -63,7 +63,7 @@ void	print_env(char **env)
 	i = 0;
 	while (env[i])
 	{
-		ft_printf("%s%s", env[i], ".");
+		ft_printf("%s\n", env[i]);
 		i++;
 	}
 }
